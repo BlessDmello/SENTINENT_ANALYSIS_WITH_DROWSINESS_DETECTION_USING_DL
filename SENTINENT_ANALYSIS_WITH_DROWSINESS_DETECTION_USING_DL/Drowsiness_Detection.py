04beea36dd4e3cdec38fdb06ec8a7c8b819c63bf
BlessDmello <89186319+BlessDmello@users.noreply.github.com>
2024-06-24 22:48:13 +0530

    Add files via upload

---
+++ SENTINENT_ANALYSIS_WITH_DROWSINESS_DETECTION_USING_DL/Drowsiness_Detection.py
@@ -0,0 +1,90 @@
+from scipy.spatial import distance
+from imutils import face_utils
+import pygame #For playing sound
+import imutils
+import dlib
+import cv2
+
+#Initialize Pygame and load music
+pygame.mixer.init()
+pygame.mixer.music.load('alert.wav')
+
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+#calculating mouth aspect ratio
+def mouth_aspect_ratio(mou):
+	# compute the euclidean distances between the horizontal
+	X   = distance.euclidean(mou[0], mou[6])
+	# compute the euclidean distances between the vertical
+	Y1  = distance.euclidean(mou[2], mou[10])
+	Y2  = distance.euclidean(mou[4], mou[8])
+	# taking average
+	Y   = (Y1+Y2)/2.0
+	# compute mouth aspect ratio
+	mar = Y/X
+	return mar
+	
+thresh = 0.25
+MOU_AR_THRESH = 0.50
+yawnStatus = False
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+cap=cv2.VideoCapture(0)
+flag=0
+while True:
+	ret, frame=cap.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	prev_yawn_status = yawnStatus
+	subjects = detect(gray, 0)
+	for subject in subjects:
+		shape = predict(gray, subject)
+		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		mouth = shape[mStart:mEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+		mouEAR = mouth_aspect_ratio(mouth)
+		ear = (leftEAR + rightEAR) / 2.0
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		mouthHull = cv2.convexHull(mouth)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+
+		if mouEAR > MOU_AR_THRESH or ear<thresh:
+			flag += 1
+			print (flag)
+			if flag >= frame_check:
+				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				cv2.putText(frame, "****************ALERT!****************", (10,325),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				# pygame.mixer.music.play(-1)
+
+				
+				#print ("Drowsy")
+		else:
+			# pygame.mixer.music.stop()
+			yawnStatus = False
+			pygame.mixer.music.play(-1)
+			flag = 0
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+cv2.destroyAllWindows()
+cap.release() 
